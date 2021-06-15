18d2f25fa2a138213cfe519b35b003b437098adc
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-06-15 09:55:12 -0300

    :recycle: refactor: change header and footer to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
 import { AppProps } from 'next/app'
+import React from 'react'
 
-import GlobalStyle from '../styles/global'
 import { ChakraProvider } from '@chakra-ui/react'
+
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import GlobalStyle from '../styles/global'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
       <GlobalStyle />
+      <Footer />
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import Head from 'next/head'
 import React from 'react'
-
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Homepage</title>
       </Head>
-
-      <Header />
-      <Footer />
     </>
   )
 }
