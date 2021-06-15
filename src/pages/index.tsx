07b13781bf342a5a23d0a8a4fe7d182c8f1bb25c
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-06-15 11:22:09 -0300

    :construction: chore: create home page's first part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,62 @@
 import Head from 'next/head'
 import React from 'react'
+
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  VStack
+} from '@chakra-ui/react'
+
+import Mouse from '../../public/mouse.svg'
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Homepage</title>
       </Head>
+      <VStack spacing={0}>
+        <HStack
+          as="main"
+          backgroundImage="gradient.svg"
+          bgPosition="right"
+          bgRepeat="no-repeat"
+          px={24}
+          pt={36}
+          spacing={32}
+          display="flex"
+          justifyContent="space-between"
+        >
+          <VStack as="section" spacing={8} display="flex" alignItems="left">
+            <Heading variant="48">Desafios e competições!</Heading>
+            <Text variant="18">
+              Desafie-se concluindo diversos desafios dos mais diversos níveis e
+              tecnologias. Compita com outros usuários com o nosso sistema de
+              ranking e pontos!
+            </Text>
+            <Button size="lg" bgColor="purple.500" w={36}>
+              Desafios
+            </Button>
+          </VStack>
+          <Flex as="section">
+            <Image src="code.svg" minW="615px" />
+          </Flex>
+        </HStack>
+        <VStack
+          display="flex"
+          spacing={4}
+          pb={10}
+          direction="column"
+          alignItems="center"
+        >
+          <Mouse />
+          <Text variant="14">Deslize para ver mais</Text>
+        </VStack>
+      </VStack>
     </>
   )
 }
