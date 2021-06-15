3ace5964cf7b37c89e9ea4468e1c2d3d5e2735f6
JuDCraide <juliadcraide31@gmail.com>
2021-06-15 01:41:35 -0300

    ✨ feat: footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Footer />
     </>
   )
 }
