4f09f1f6b8913d3a9ceba143dbc5e15c16cffb15
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2021-06-16 22:32:52 -0300

    :sparkles: feat: integrate list challenges

--- src/pages/challenges.tsx
+++ src/pages/challenges.tsx
@@ -1,3 +1,7 @@
+import { GetStaticProps } from 'next'
+import Prismic from '@prismicio/client'
+import { getPrismicClient } from '../services/prismic'
+import { RichText } from 'prismic-dom'
 import React, { useEffect, useState } from 'react'
 
 import {
@@ -42,15 +46,38 @@ const techOptions: Option[] = [
   }
 ]
 
-export default function Challenges() {
+interface Challenge {
+  slug: string
+  title: string
+  banner: string
+  technology: string
+  difficulty: string
+}
+
+interface ChallengesProps {
+  challenges: Challenge[]
+}
+
+export default function Challenges({ challenges }: ChallengesProps) {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [showChallenges, setShowChallenges] = useState<Challenge[]>([])
+
   const [level, setLevel] = useState<string>()
   const [tech, setTech] = useState<string>()
-
-  const elements = [1, 2, 3, 4, 5, 6]
 
   useEffect(() => {
     eva.replace()
   }, [])
+
+  useEffect(() => {
+    const newChallenges: Challenge[] = []
+    for (let index = (currentPage - 1) * 8; index < 8 * currentPage; index++) {
+      if (challenges[index]) {
+        newChallenges.push(challenges[index])
+      }
+    }
+    setShowChallenges(newChallenges)
+  }, [currentPage])
 
   return (
     <Flex py={12} px={28} direction="column">
@@ -72,19 +99,20 @@ export default function Challenges() {
         />
       </HStack>
       <SimpleGrid columns={3} spacing={8} minChildWidth="340px">
-        {elements.map(element => {
+        {showChallenges.map(challenge => {
           return (
             <VStack
-              key={element}
+              key={challenge.slug}
               bgColor="gray.800"
               borderRadius="11px"
               display="flex"
               alignItems="left"
               p={6}
               spacing={6}
+              maxW={389}
             >
               <Image
-                src="https://via.placeholder.com/340"
+                src={challenge.banner}
                 borderRadius="11"
                 h={40}
                 w="full"
@@ -92,18 +120,49 @@ export default function Challenges() {
               />
               <HStack spacing={6}>
                 <Box bgColor="purple.500" px={4} py={1} borderRadius="11px">
-                  <Heading variant="14">ReactJS</Heading>
+                  <Heading variant="14">{challenge.technology}</Heading>
                 </Box>
                 <Box bgColor="pink.500" px={4} py={1} borderRadius="11px">
-                  <Heading variant="14">Hard</Heading>
+                  <Heading variant="14">{challenge.difficulty}</Heading>
                 </Box>
               </HStack>
-              <Heading variant="24">Título desafio {element}</Heading>
+              <Heading variant="24">{challenge.title}</Heading>
             </VStack>
           )
         })}
       </SimpleGrid>
-      <Pagination totalCountOfRegisters={6} />
+      <Pagination
+        totalCountOfRegisters={challenges.length}
+        registersPerPage={8}
+        currentPage={currentPage}
+        onPageChange={setCurrentPage}
+      />
     </Flex>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'challenge')],
+    {
+      pageSize: 100
+    }
+  )
+
+  const challenges = response.results.map(challenge => {
+    return {
+      slug: challenge.uid,
+      title: RichText.asText(challenge.data.title),
+      banner: challenge.data.banner.url,
+      technology: challenge.data.technology,
+      difficulty: challenge.data.difficulty
+    }
+  })
+
+  return {
+    props: {
+      challenges
+    }
+  }
 }
