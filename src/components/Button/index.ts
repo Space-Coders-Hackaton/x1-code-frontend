2a401eeccab8ddac44dd6ef299a95949bb28edf6
Gilvan Araújo <gilvanjunior@protonmail.com>
2021-06-17 14:23:19 -0300

    :fire: refactor: remove button component

--- src/components/Button/index.ts
+++
@@ -1,9 +0,0 @@
-import { Solid } from './Solid'
-import { Transparent } from './Transparent'
-import { OutlinePink } from './OutlinePink'
-
-export default {
-  Solid,
-  Transparent,
-  OutlinePink
-}
