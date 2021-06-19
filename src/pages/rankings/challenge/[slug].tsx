08d037c18c8175242c12fa572c18e1dd8aca82a8
allyfx <aliciafoureauxlucas@gmail.com>
2021-06-19 11:39:04 -0300

    :sparkles: list challenge rank

--- src/pages/rankings/challenge/[slug].tsx
+++ src/pages/rankings/challenge/[slug].tsx
@@ -1,71 +1,188 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import { RichText } from 'prismic-dom'
 
-import { Flex, Heading, HStack, Link, Text, VStack } from '@chakra-ui/layout'
-import { Tooltip } from '@chakra-ui/tooltip'
-import { InfoIcon } from '@chakra-ui/icons'
+import { getPrismicClient } from '../../../services/prismic'
+import { useSelector } from 'react-redux'
+
+import { Store } from '../../../store'
+import { User } from '../../../store/modules/user/types'
+
+import { Flex, Heading, HStack, Link, VStack } from '@chakra-ui/layout'
 import { PodiumItem } from '../../../components/Ranking/PodiumItem'
 import { ListItem } from '../../../components/Ranking/ListItem'
-import { useRouter } from 'next/dist/client/router'
 
-export default function Rankings() {
-  const userRanks = [4, 5, 6, 7, 8, 9, 10]
+import { api } from '../../../services/api'
+
+interface UserRank {
+  points: number
+  totalChallenges: number
+  user: {
+    id: string
+    name: string
+  }
+  position?: number
+}
+
+interface RankingsProps {
+  challenge: string
+}
+
+export default function Rankings({ challenge }: RankingsProps) {
   const router = useRouter()
-  const { challenge } = router.query // Destructuring our router object
+  const { slug } = router.query
+
+  const { id } = useSelector<Store, User>(state => state.user)
+
+  const [users, setUsers] = useState<UserRank[]>([])
+  const [principalUsers, setPrincipalUsers] = useState<UserRank[]>([])
+  const [listUsers, setListUsers] = useState<UserRank[]>([])
+
+  function filterShowUsers() {
+    const newPrincipalUser: UserRank[] = []
+    const newListUser: UserRank[] = []
+
+    for (let index = 0; index <= 2; index++) {
+      if (users[index]) {
+        newPrincipalUser.push({
+          ...users[index],
+          position: index + 1
+        })
+      }
+    }
+
+    if (users.length > 3) {
+      const loggedUser = users.findIndex(rank => rank.user.id === id)
+
+      for (let index = 0; index <= 9; index++) {
+        if (users[index]) {
+          newListUser.push({
+            ...users[index],
+            position: index + 1
+          })
+        }
+      }
+
+      if (loggedUser !== -1 && loggedUser + 1 > newListUser.length) {
+        newListUser.push({
+          ...users[loggedUser],
+          position: loggedUser + 1
+        })
+      }
+    }
+
+    setPrincipalUsers(newPrincipalUser)
+    setListUsers(newListUser)
+  }
+
+  async function getUsers() {
+    try {
+      const { data } = await api.get(`/ranking/challenge/${slug}`)
+
+      setUsers(data)
+      filterShowUsers()
+    } catch (err) {
+      // Toast
+
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <Flex direction="column" px={28} py={14}>
       <Head>
-        <title>[Slug] | X1 Code</title>
+        <title>{challenge} | X1 Code</title>
       </Head>
 
       <Flex direction="row" justifyContent="space-between" pb={5}>
-        <Heading variant="48">Ranking desafio {challenge}</Heading>
-        <HStack spacing={4}>
-          <Text variant="18">Como funciona</Text>
-          <Tooltip
-            hasArrow
-            label="Um ranking de todos os usuários da aplicação que já enviaram soluções de desafios baseado na pontuação recebida."
-            fontSize="md"
-          >
-            <InfoIcon />
-          </Tooltip>
-        </HStack>
+        <Heading variant="48">{challenge}</Heading>
       </Flex>
-      <HStack
-        spacing={40}
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-end"
-      >
-        <PodiumItem name="John Doe" place={2} score={30} />
-        <PodiumItem name="John Doe" place={1} score={30} />
-        <PodiumItem name="John Doe" place={3} score={30} />
-      </HStack>
-      <VStack mt={12} spacing={6}>
-        {userRanks.map(userRank => {
-          return (
-            <ListItem
-              key={userRank}
-              rank={userRank}
-              name={`User ${userRank}`}
-              score={userRank * 10}
-            />
-          )
-        })}
-        <ListItem rank={89} name="John Doe" score={30} isUser />
-      </VStack>
-      <Link alignSelf="center">
-        <Heading
-          variant="24"
-          color="purple.500"
-          mt={12}
-          mb={48}
-          _hover={{ textDecoration: 'underline' }}
-        >
-          Ver mais
+      {users.length > 0 && (
+        <>
+          <HStack
+            spacing={40}
+            display="flex"
+            justifyContent="center"
+            alignItems="flex-end"
+          >
+            {principalUsers[1] && (
+              <PodiumItem
+                name={principalUsers[1].user.name}
+                place={2}
+                score={principalUsers[1].points}
+              />
+            )}
+            {principalUsers[0] && (
+              <PodiumItem
+                name={principalUsers[0].user.name}
+                place={1}
+                score={principalUsers[0].points}
+              />
+            )}
+            {principalUsers[2] && (
+              <PodiumItem
+                name={principalUsers[2].user.name}
+                place={3}
+                score={principalUsers[2].points}
+              />
+            )}
+          </HStack>
+          <VStack mt={12} spacing={6}>
+            {listUsers.map(rank => {
+              return (
+                <ListItem
+                  key={rank.user.id}
+                  rank={rank.position}
+                  name={rank.user.name}
+                  score={rank.points}
+                  isUser={rank.user.id === id}
+                />
+              )
+            })}
+          </VStack>
+          {users.length > listUsers.length && (
+            <Link alignSelf="center">
+              <Heading
+                variant="24"
+                color="purple.500"
+                mt={12}
+                mb={48}
+                _hover={{ textDecoration: 'underline' }}
+              >
+                Ver mais
+              </Heading>
+            </Link>
+          )}
+        </>
+      )}
+
+      {users.length <= 0 && (
+        <Heading p={28} alignSelf="center" variant="18">
+          Nenhum usuário ;-;
         </Heading>
-      </Link>
+      )}
     </Flex>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params
+}) => {
+  const { slug } = params
+  const prismic = getPrismicClient(req)
+  const response = await prismic.getByUID('challenge', String(slug), {})
+
+  const challenge = RichText.asText(response.data.title)
+  return {
+    props: {
+      challenge
+    }
+  }
 }
